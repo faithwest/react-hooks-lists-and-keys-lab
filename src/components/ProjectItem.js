642bea217f5281b6c82a6817fbb67351673ec49a
faithwest <faithchepkoech425@gmail.com>
2023-10-25 23:32:28 +0300

    assignment done

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,15 +1,23 @@
 import React from "react";
+import ProjectList from "./ProjectList";
 
-function ProjectItem({ name, about, technologies }) {
+function ProjectItem(props) {
+  const { projectName, technologies } = props;
+
   return (
-    <div className="project-item">
-      <h3>{name}</h3>
-      <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+    <div className="project-card">
+      <h2>{projectName}</h2>
+      <div className="platforms">
+        {technologies && Array.isArray(technologies) ? (
+          technologies.map((technology, index) => (
+            <span key={index}>{technology}</span>
+          ))
+        ) : (
+          <p>No technologies available</p>
+        )}
       </div>
     </div>
   );
 }
-
 export default ProjectItem;
+
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,14 +1,21 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
 
-function ProjectList({ projects }) {
-  console.log(projects);
+function ProjectList(props) {
+  
+  const { projects } = props;
+
   return (
-    <div id="projects">
-      <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+    <div> 
+      <h2>Projects</h2>  
+      <ul>
+        {projects.map((project) => (
+          <ProjectItem key={project.id} project={project} />
+        ))}
+      </ul>
     </div>
   );
 }
 
 export default ProjectList;
+
